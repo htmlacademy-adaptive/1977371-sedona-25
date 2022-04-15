563324e853bbecb08e60d76fea37dc015f4c0047
Keks <keks@htmlacademy.ru>
2022-04-15 20:10:16 +0300

    🦊 Добавляет автоматизацию

---
+++ source/templates/_data.js
@@ -0,0 +1,20 @@
+export default {
+  "pages": [
+    {
+      "path": "index.html",
+      "name": "Главная"
+    },
+    {
+      "path": "catalog.html",
+      "name": "Фото и видео"
+    },
+    {
+      "path": "form.html",
+      "name": "Форма отзыва"
+    },
+    {
+      "path": "https://htmlacademy.ru/intensive/adaptive",
+      "name": "HTML Academy"
+    }
+  ]
+}
