e5edbbdfa7d4f4ebd2cea5878eb7b8b5eb556454
Евгений Калинин <97776034+EvgeniyDobrov@users.noreply.github.com>
2022-05-07 18:33:05 +0600

    фотографии на главныйх страницах

--- source/templates/_data.js
+++ source/templates/_data.js
@@ -16,5 +16,29 @@ export default {
       "path": "https://htmlacademy.ru/intensive/adaptive",
       "name": "HTML Academy"
     }
-  ]
+  ],
+  "viewports": {
+    "desktop": 1280,
+    "tablet": 768,
+    "mobile": null
+  },
+  "images": {
+    "photo-city": {
+      "ext": "jpg",
+      "width": 520,
+      "height": 260,
+      "maxdppx": 2,
+      "alt": "Фотография Седоны",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "photo-bridge": {
+      "ext": "jpg",
+      "width": 520,
+      "height": 260,
+      "maxdppx": 2,
+      "alt": "Фотография моста",
+      "viewports": ["desktop", "tablet", "mobile"]
+    }
+  }
 }
+
