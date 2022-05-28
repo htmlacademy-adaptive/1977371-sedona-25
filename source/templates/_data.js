7d9fea0efd20dffe07d985cdae57eb1a152f998e
Евгений Калинин <97776034+EvgeniyDobrov@users.noreply.github.com>
2022-05-28 05:26:16 +0300

    8:26

--- source/templates/_data.js
+++ source/templates/_data.js
@@ -23,12 +23,20 @@ export default {
     "mobile": null
   },
   "images": {
+    "map": {
+      "ext": "jpg",
+      "width": 320,
+      "height": 350,
+      "maxdppx": 2,
+      "alt": "Карта городка Седона.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
     "photo-city": {
       "ext": "jpg",
       "width": 520,
       "height": 260,
       "maxdppx": 2,
-      "alt": "Фотография Седоны",
+      "alt": "Фотография Седоны.",
       "viewports": ["desktop", "tablet", "mobile"]
     },
     "photo-bridge": {
@@ -36,7 +44,47 @@ export default {
       "width": 520,
       "height": 260,
       "maxdppx": 2,
-      "alt": "Фотография моста",
+      "alt": "Фотография моста.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/photo-1": {
+      "ext": "jpg",
+      "width": 280,
+      "height": 280,
+      "maxdppx": 2,
+      "alt": "Народные просторы.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/photo-2": {
+      "ext": "jpg",
+      "width": 280,
+      "height": 280,
+      "maxdppx": 2,
+      "alt": "Местная растительность.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/photo-3": {
+      "ext": "jpg",
+      "width": 280,
+      "height": 280,
+      "maxdppx": 2,
+      "alt": "Дорога на север.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/photo-4": {
+      "ext": "jpg",
+      "width": 280,
+      "height": 280,
+      "maxdppx": 2,
+      "alt": "Мост дьявола.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog/video": {
+      "ext": "jpg",
+      "width": 280,
+      "height": 177,
+      "maxdppx": 2,
+      "alt": "Превью видео.",
       "viewports": ["desktop", "tablet", "mobile"]
     }
   }
